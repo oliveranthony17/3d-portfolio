9cb7b29b538d1f7772b5e28b1ffb9e96dbef5b4d
oliveranthony17 <oliver_anthony@outlook.com>
2023-03-15 22:35:39 +0100

    initial commit for deployment on vercel

---
+++ src/components/Hero.jsx
@@ -0,0 +1,47 @@
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
+
+const Hero = () => {
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row item-start gap-5`}>
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Oliver</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a full-stack developer with an array of knowledge in many frontend and backend languages, responsive frameworks, databases, and best code practices. My objective is simply to be the best developer that I can be and to wake up every day excited to code.
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[65px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
+            {/* below uses framer-motion to create "button" you can use to scroll */}
+            <motion.div
+              animate={{
+                y: [0, 30, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-white mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,78 @@
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
+
+const Computers = ({isMobile}) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor="black" />
+      <pointLight
+        intensity={1.5}
+        position={[-0.1, -0.5, 1]}
+      />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1.5}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [0, -3, -0.8] : [0, -3, -1.25]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // below changes isMobile variable when window width falls below 768px
+  useEffect(() => {
+    // add a listener for changes to screen size
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+    // set the initial value of the 'isMobile' state variable
+    setIsMobile(mediaQuery.matches);
+    // define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    // add the callback fuction as a listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    // remove the listener when the component unmounts
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 40 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
